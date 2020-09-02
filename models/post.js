9259e5741469e0ec3bed913877c252180f64e1c1
abhinav <abhiut37@gmail.com>
2020-09-02 13:49:46 +0530

    added post route

---
+++ models/post.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const postSchema = new schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+    },
+    avatar: {
+        type: String,
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: "users",
+            },
+        },
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: "users",
+            },
+            text: {
+                type: String,
+                required: true,
+            },
+            date: {
+                type: Date,
+                default: Date.now,
+            },
+        },
+    ],
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = Posts = mongoose.model("posts", postSchema);
--- routes/apis/posts.js
+++ routes/apis/posts.js
@@ -1,9 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const Posts = require("../../models/post");
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
+const {
+    check,
+    valitadionResult,
+    validationResult,
+} = require("express-validator/check");
+const auth = require("../../middleware/auth");
 
-//@route get api/posts
+//@route POST api/posts
 //@desc  for authentication
-//@access public
-router.get("/", (req, res) => res.send("posts route.."));
+//@access Private
+router.get(
+    "/",
+    [auth, [check("text", "text is requred").not().isEmpty()]],
+    async (req, res) => {
+        const error = validationResult(req);
+        if (!error.isEmpty()) {
+            return res.status("400").json({ error: error.array() });
+        }
+        try {
+            let user = (await User.findById(req.user.id)).isSelected(
+                "-password"
+            );
+            const newPost = {
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+            };
+
+            const post = new Posts(newPost);
+            await post.save();
+            res.json(post);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("server error");
+        }
+    }
+);
 
 module.exports = router;
